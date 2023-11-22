38e855a9efe2b3d4ef5888ea873e8161d5ae47bf
Sichuich <120375094+Sichuich@users.noreply.github.com>
2023-11-22 12:26:57 +0100

    Add files via upload

---
+++ Service/cap.c
@@ -0,0 +1,19 @@
+#include "stm32f10x.h"
+#include "MyTimer.h"
+#include "Driver_GPIO.h"
+
+void commande(int recu){
+	/*int Rapport;	
+	
+	//recu 
+	Rapport = (recu)
+	
+
+	 if (Rapport < 0) {
+        MyGPIO_Set(GPIOA, 6);
+		 Rapport = (-1) * Rapport ; 
+    } else {
+        MyGPIO_Reset(GPIOA, 6);
+    }					
+    MyTimer_SetDutyCycle(TIM3, 1, Rapport);*/
+}
---
+++ Service/cap.h
@@ -0,0 +1,9 @@
+#include "stm32f10x.h"
+#include "MyTimer.h"
+#include "Driver_GPIO.h"
+
+//
+void commande(int Rapport);
+
+void RouteurInit();
+
