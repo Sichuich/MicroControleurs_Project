f41daab9169e79ea5604074f0754bf01f7994c6f
lauracab31 <147844898+lauracab31@users.noreply.github.com>
2023-12-08 17:31:09 +0100

    Add files via upload

--- Drivers/Driver_UART.h
+++ Drivers/Driver_UART.h
@@ -1,3 +1,6 @@
+#ifndef MYUART_H
+#define MYUART_H
+
 #include "stm32f10x.h"
 #include "Driver_GPIO.h"
 #include "MyTimer.h"
@@ -5,3 +8,9 @@
 void Uart_init(USART_TypeDef * Uart,unsigned int baudrate);
 
 void Send(USART_TypeDef * Uart, char data);
+
+void Receive_Interruption(USART_TypeDef * usart, char priority,  void (*function) (void));
+
+char Uart_Get(USART_TypeDef * usart);
+
+#endif
