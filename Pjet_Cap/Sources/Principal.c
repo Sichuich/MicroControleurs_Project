7e81cda20dd98cf447b009405c5577d27126f83e
lauracab31 <147844898+lauracab31@users.noreply.github.com>
2023-12-08 17:32:12 +0100

    Add files via upload

--- Pjet_Cap/Sources/Principal.c
+++ Pjet_Cap/Sources/Principal.c
@@ -1,11 +1,26 @@
 #include "stm32f10x.h"
+#include "MyTimer.h"
 #include "Driver_GPIO.h"
-int main(void) 
-{
-	
-	RouteurInit();
-	RouteurSet(50);
-	RouteurStart();
-	while(1);
+#include "Driver_UART.h"
+#include "cap.h"
 
+
+char received_data;
+
+void Receive(void){
+	received_data = Uart_Get(USART1);
+	Send(USART1, received_data);
+}
+
+
+int main(void) {
+	
+    RouteurInit();
+	  Receive_Interruption(USART1,8,Receive);  
+	  RouteurInit();
+	  RouteurStart();
+    
+    while (1) { 
+   	  RouteurSet(received_data);   
+    }
 }
